cb8fe22ed1b1d56ec78c24567f2fd721943413ec
unknown <wadoudazer1234@gmail.com>
2025-06-09 19:53:03 +0100

    try to fix cors

--- dist/app.js
+++ dist/app.js
@@ -18,7 +18,7 @@ async function startServer() {
     const app = (0, express_1.default)();
     // app.use(errorHandler);
     app.use((0, cookie_parser_1.default)());
-    app.use((0, cors_1.default)({ origin: "http://localhost:3000", credentials: true }));
+    app.use((0, cors_1.default)({ origin: "*", credentials: true }));
     app.use((0, helmet_1.default)());
     app.use((0, morgan_1.default)('dev'));
     app.use(express_1.default.json({ limit: '10mb' }));
